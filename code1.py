1c8fc361e7cd2a014875a005f952d1a7fb0a53e3
Athul Sajeevan <athulsajeevan01@gmail.com>
2023-02-23 22:23:41 +0530

    sixth commit

--- code1.py
+++ code1.py
@@ -3,7 +3,17 @@ import cv2
 cam = cv2.VideoCapture(0) #object creation,by class
 
 count=0
-a, img = cam.read() #image array,a status  T/F
-cv2.imshow("Camera", img)
-key = cv2.waitKey(1) #frame holding time
-print(key)
+while True: 
+
+    a, img = cam.read() #image array,a status  T/F
+    
+    cv2.imshow("Camera", img)
+    key = cv2.waitKey(1) #frame holding time
+    print(key)
+    if(key == ord('q')): #to return asci value
+        imgname=str(count)+".png"
+        cv2.imwrite(imgname,img)
+        count=count+1
+        
+        print("Captured")
+        break
