8c223797592000fe7987deaebec4e58fc3be5303
Smijo-Saji <68843252+Smijo-Saji@users.noreply.github.com>
2023-02-23 21:46:35 +0530

    Update codes1.py

--- codes1.py
+++ codes1.py
@@ -4,3 +4,8 @@ img = cv2.imread('0.png')
 cv2.imshow("Color Image", img) #frame
 imgsize=img.shape
 print("Color Image",imgsize)
+
+gray_image=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imshow("gray Image", gray_image)
+imgsize1=gray_image.shape
+print("Gray Image",imgsize1)
