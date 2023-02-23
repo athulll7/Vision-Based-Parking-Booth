72543f2aebb75558297a455d5de975a2831d236b
Athul Sajeevan <athulsajeevan01@gmail.com>
2023-02-23 22:22:28 +0530

    fifth commit

--- code1.py
+++ code1.py
@@ -4,3 +4,6 @@ cam = cv2.VideoCapture(0) #object creation,by class
 
 count=0
 a, img = cam.read() #image array,a status  T/F
+cv2.imshow("Camera", img)
+key = cv2.waitKey(1) #frame holding time
+print(key)
