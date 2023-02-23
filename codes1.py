705c1f380c2931ad4a7a4a6aaf22d7d3bff8cebf
Smijo-Saji <68843252+Smijo-Saji@users.noreply.github.com>
2023-02-23 21:47:01 +0530

    Update codes1.py

--- codes1.py
+++ codes1.py
@@ -9,3 +9,8 @@ gray_image=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 cv2.imshow("gray Image", gray_image)
 imgsize1=gray_image.shape
 print("Gray Image",imgsize1)
+
+if(len(imgsize1)==3):
+    print("Color Image")
+elif(len(imgsize1)==2):
+    print("Gray Image")
