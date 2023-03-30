a0549183b252084f2aa8ecdb26899c97bb8e25e7
Smijo-Saji <Smijo-Saji>
2023-03-30 22:53:23 +0530

    test2

--- vggtraintest.py
+++ vggtraintest.py
@@ -1,2 +1,15 @@
 from google.colab import drive
 drive.mount('/content/drive')
+import numpy as np 
+import matplotlib.pyplot as plt
+import os
+import tensorflow
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import Dense, Input, Dropout,Flatten, Conv2D
+from tensorflow.keras.layers import BatchNormalization, Activation, MaxPooling2D
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
+from tensorflow.keras.utils import plot_model
+from IPython.display import SVG, Image
